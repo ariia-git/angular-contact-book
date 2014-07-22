2185bb7f2dedf40fcc206726133cd0162d1aa605
sudodoki <smd.deluzion@gmail.com>
2014-07-22 09:39:51 +0300

    WIP

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -6,37 +6,18 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
+  describe('initial state', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render have filter input', function() {
+      expect(element(by.css('input.search')).length).toBeGreaterThan(0)
     });
 
   });
+
+
 });
